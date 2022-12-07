5f8ec60be1a4da2c342b4a47462c2d66406d0777
Lucas Baach <lucasbaach1@gmail.com>
2022-12-07 13:52:14 -0300

    primeiro exercicio do conjunto de exercicos do 6

---
+++ Back-end/secao-4-Introdução-ao-desenvolvimento-Web-com-Node.js/Exercicio-6-modulo-fs/simpsons.js
@@ -0,0 +1,20 @@
+//A - Crie uma função que leia todos os dados do arquivo e imprima cada personagem no formato id - Nome. Por exemplo: 1 - Homer Simpson.
+
+const fs = require('fs').promises;
+const path = require('path');
+
+async function readingJson () {
+
+        const readSimpsons = await fs.readFile('./simpsons.json', 'utf-8');
+        const simp = JSON.parse(readSimpsons);
+        const strings = simp.map(({ id, name }) => `${id} - ${name}`);
+        strings.forEach(element => console.log(element));
+};
+
+async function main () {
+    await readingJson();
+}
+
+main()
+
+//B - Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com os dados da personagem que possui o id informado. Caso não haja uma personagem com o id informado, rejeite a Promise com o motivo “id não encontrado”.
